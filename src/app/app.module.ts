4fdc52954b6b082c1735a91175d3bf3bec7b4879
Romina Painter <romina.painter@stukent.com>
2019-09-27 21:06:11 -0600

    Finished header and added the components needed, following video tutorial and working on the assignment at the same time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
